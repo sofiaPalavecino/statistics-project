ea918c36f505d86ae61423ebe9408f7c6495482b
sofiaPalavecino <sofiaor29@gmail.com>
2025-01-15 13:58:16 -0300

    modified db schema

--- db/statistics_db.sql
+++ db/statistics_db.sql
@@ -4,7 +4,7 @@ USE statistics;
 
 CREATE TABLE IF NOT EXISTS player_stats (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    player_id INT NOT NULL,
+    player_id VARCHAR(255) NOT NULL,
     nickname VARCHAR(255),
     profile_image VARCHAR(255),
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -12,12 +12,6 @@ CREATE TABLE IF NOT EXISTS player_stats (
 );
 
 INSERT INTO player_stats (player_id, nickname, profile_image, score)
-VALUES (12, "varkirion", "https://pbs.twimg.com/profile_images/1481003172613472257/nTgW4oHu_400x400.jpg", 100),
-(13, "purpledog163", "https://pbs.twimg.com/profile_images/1481003172613472257/nTgW4oHu_400x400.jpg", 45),
-(14, "happygoose784", "https://pbs.twimg.com/profile_images/1481003172613472257/nTgW4oHu_400x400.jpg", 50),
-(15, "tinylion243", "https://pbs.twimg.com/profile_images/1481003172613472257/nTgW4oHu_400x400.jpg", 70),
-(16, "tinyelephant218", "https://pbs.twimg.com/profile_images/1481003172613472257/nTgW4oHu_400x400.jpg", 20),
-(17, "brownbird424", "https://pbs.twimg.com/profile_images/1481003172613472257/nTgW4oHu_400x400.jpg", 48),
-(18, "sadpanda509", "https://pbs.twimg.com/profile_images/1481003172613472257/nTgW4oHu_400x400.jpg", 53),
-(19, "crazypanda220", "https://pbs.twimg.com/profile_images/1481003172613472257/nTgW4oHu_400x400.jpg", 7),
-(11, "ticklishostrich889", "https://pbs.twimg.com/profile_images/1481003172613472257/nTgW4oHu_400x400.jpg", 68)
+VALUES (12, "varkirion", "https://randomuser.me/api/portraits/thumb/men/79.jpg", 1),
+(13, "purpledog163", "https://randomuser.me/api/portraits/thumb/women/1.jpg", 45),
+(14, "happygoose784", "https://randomuser.me/api/portraits/thumb/women/90.jpg", 50)
